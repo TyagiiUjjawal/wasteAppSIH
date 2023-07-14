7ab35dba6d98d940c801ed238261de269beceeff
Ujjawal Tyagi <tyagiujjawal9@gmail.com>
2023-07-14 18:27:02 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/eco",{
-   
-}).then(()=>{
+// mongoose.connect("mongodb://localhost:27017/eco",{
+mongoose
+  .connect(
+    "mongodb+srv://admin:fP6tKScVcL98ZRdP@cluster0.lhnaw.mongodb.net/Ecommerce",
+    {}
+  )
+  .then(() => {
     console.log("conn succ");
-
-}).catch((e)=>{
-    console.log("faf");
-})
-
+  })
+  .catch((e) => {
+    console.log("faf", e);
+  });
